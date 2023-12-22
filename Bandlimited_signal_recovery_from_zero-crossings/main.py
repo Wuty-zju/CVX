c90d42f3a95c1b4f4214a510a818df8ceabce76b
吴天宇 <86960011+wutianyu1129@users.noreply.github.com>
2023-12-22 13:53:49 +0000

    1

--- Bandlimited_signal_recovery_from_zero-crossings/main.py
+++ Bandlimited_signal_recovery_from_zero-crossings/main.py
@@ -1,7 +1,9 @@
 import numpy as np
 import cvxpy as cvx
 import matplotlib.pyplot as plt
-from module.zero_crossings_data import *
+from module.zero_crossings_data import zero_crossings_data
+
+n, f_min, B, s, y = zero_crossings_data()
 
 # 构造矩阵A，其列为带限正弦波
 C = np.zeros((n, B))
@@ -16,15 +18,33 @@ x = cvx.Variable(2 * B)
 obj = cvx.norm(A @ x)
 constraints = [cvx.multiply(s, A @ x) >= 0, s.T @ (A @ x) == n]
 problem = cvx.Problem(cvx.Minimize(obj), constraints)
+
+# 创建一个回调函数，用于在每次迭代后记录误差
+errors = []
+def callback(x):
+    y_hat = A @ x
+    error = np.linalg.norm(y - y_hat) / np.linalg.norm(y)
+    errors.append(error)
+
 problem.solve(solver=cvx.ECOS)
 
 y_hat = A @ x.value
-print('Recovery error: {}'.format(np.linalg.norm(y - y_hat) / np.linalg.norm(y)))
+#print('Recovery error: {}'.format(np.linalg.norm(y - y_hat) / np.linalg.norm(y)))
+
+plt.figure()
+plt.plot(errors, label='误差')
+plt.xlabel('迭代次数')
+plt.ylabel('相对恢复误差')
+plt.title('误差与迭代次数')
+plt.legend()
+plt.show()
 
 plt.figure(figsize=(24, 5))
-plt.plot(np.arange(0, n), y, label='original');
-plt.plot(np.arange(0, n), y_hat, label='recovered');
-plt.xlim([0, n])
+plt.plot(y, label='original');
+plt.plot(y_hat, label='recovered');
+#plt.plot(np.arange(0, n), y, label='original');
+#plt.plot(np.arange(0, n), y_hat, label='recovered');
+#plt.xlim([0, n])
 plt.legend(loc='lower left')
 plt.tight_layout()
 plt.savefig('./Bandlimited_signal_recovery_from_zero-crossings/outputs/original_and_recovered_y_signal.jpg')
